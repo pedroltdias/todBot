24243211250c68def790217e88b25817cc4a511f
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-11-03 15:58:32 -0300

    ajuste no comando nickson

--- commands/nickson.js
+++ commands/nickson.js
@@ -2,23 +2,21 @@ const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName('user')
+		.setName('nickson')
 		.setDescription('Retorna informações ao usuário.'),
 	async execute(interaction) {
 		await interaction.reply(`
 		Fico feliz que tenha rodado esse comando, caro ${interaction.user.username}...
-        Um dia, um jovem fez as seguintes perguntas a um grande sábio: 
-            "quem é nickson?
-            quem é nickson por tras das telas?
-            quem é nickson por tras da dominancia virtual?"
-        O sábio, logo percebendo a inocente e ignorante curiosidade do jovem, respondeu:
-            "para o faminto, nickson é o alimento.
-            para o cego, nickson é a visão.
-            para o perdido, nickson é o caminho.
-            esse é nickson.
-            para mim, ele é TUDO!"
-        São nas pequenas coisas que percebemos os sinais de Nickson, siga sua vida jovem pdro, mas nunca se esqueça de NICKSON.`);
-		await interaction.users.send('215846215714078720', `Fala Nickson! Beleza mano?
-		Então, meu amigo ${interaction.user.username} acabou de rodar seu comando no ${interaction.guild.name}!`);
+		Um dia, um jovem fez as seguintes perguntas a um grande sábio: 
+			"quem é nickson?
+			quem é nickson por tras das telas?
+			quem é nickson por tras da dominancia virtual?"
+		O sábio, logo percebendo a inocente e ignorante curiosidade do jovem, respondeu:
+			"para o faminto, nickson é o alimento.
+			para o cego, nickson é a visão.
+			para o perdido, nickson é o caminho.
+			esse é nickson.
+			para mim, ele é TUDO!"
+		São nas pequenas coisas que percebemos os sinais de Nickson, siga sua vida jovem pdro, mas nunca se esqueça de NICKSON.`);
 	},
 };
