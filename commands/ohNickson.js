97b9bdd639d2806e36a20a634172c08af1c7c5e1
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-11-03 16:08:25 -0300

    ohNickson nao manda msg privada, agora manda no chat

--- commands/ohNickson.js
+++ commands/ohNickson.js
@@ -5,7 +5,6 @@ module.exports = {
 		.setName('ohnickson')
 		.setDescription('Retorna informações ao usuário.'),
 	async execute(interaction) {
-		await interaction.users.send('215846215714078720', `Fala Nickson! Beleza mano?
-		Então, meu amigo ${interaction.user.username} acabou de rodar seu comando no ${interaction.guild.name}!`);
+		await interaction.reply(`Fala Nickson! Beleza mano? <@${'215846215714078720'}>. Então, meu amigo ${interaction.user.username} acabou de rodar seu comando no ${interaction.guild.name}!`);
 	},
 };
